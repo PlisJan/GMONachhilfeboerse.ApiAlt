1ed1d44a35c477331997a3f077bae7fe0e6bdddb
Jan Plischke <mail@j-plis.de>
2022-05-10 23:31:41 +0200

    Moved DB Data to Env-File

--- database.py
+++ database.py
@@ -1,23 +1,15 @@
-# import os
-
-# from sqlalchemy.engine import create_engine
-# from sqlalchemy.ext.declarative import declarative_base
-# from sqlalchemy.orm.session import sessionmaker
-
-# DBhost = "127.0.0.1"  # os.environ.get("dbHost")
-# engine = create_engine(
-#     'mysql://nachhilfeboerse:Nachhilfeboerse@{}:3306/nachhilfeboerse'.format(DBhost))  # TODO: Store in env var
-# Base = declarative_base()
-# Session = sessionmaker(engine)
-
 import os
-
 from sqlalchemy.engine import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm.session import sessionmaker
 
-DBhost = "v2202110158863166299.supersrv.de"  # os.environ.get("dbHost")
+DB_HOST = os.environ.get("DB_HOST")
+DB_PORT = os.environ.get("DB_PORT")
+DB_USERNAME = os.environ.get("DB_USERNAME")
+DB_PASSWORD = os.environ.get("DB_PASSWORD")
+DB_SCHEMA = os.environ.get("DB_SCHEMA")
+
 engine = create_engine(
-    'mysql://nachhilfeboerse:Nachhilfeboerse@{}:3306/nachhilfeboerse'.format(DBhost))  # TODO: Store in env var
+    f'mysql://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_SCHEMA}')
 Base = declarative_base()
 Session = sessionmaker(engine)
