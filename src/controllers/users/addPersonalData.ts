66ba7012d178da71c39f52e1059d93ff668d5405
Jan Plischke <privat@j-plis.de>
2022-09-14 19:12:43 +0200

    added documetation for addPersonalData

--- src/controllers/users/addPersonalData.ts
+++ src/controllers/users/addPersonalData.ts
@@ -4,6 +4,75 @@ import Joi from "joi";
 import { query } from "services/db";
 import validationPatterns from "validation/commonPatterns";
 
+/**
+ * @swagger
+ * /user/addPersonalData:
+ *    post:
+ *      tags:
+ *        - user
+ *      summary: Add Name, Email and Phonenumber
+ *      description: Add/ Change Name, Email and Phonenumber from the currentt user
+ *      operationId: addPersonalData
+ *      requestBody:
+ *        description: Provide Name, Email and Phonenumber
+ *        content:
+ *          application/json:
+ *            schema:
+ *              properties:
+ *                name:
+ *                  type: string
+ *                  pattern: "\\s[^ ]"
+ *                  example: Max Mustermann
+ *                email:
+ *                  type: string
+ *                  format: email
+ *                  example: max.mustermann@email.tld
+ *                phonenumber:
+ *                  type: string
+ *                  example: +49 0000 0000000
+ *        required: true
+ *
+ *      responses:
+ *        "200":
+ *          description: Successfully updated email
+ *          content:
+ *            application/json:
+ *              schema:
+ *                properties:
+ *                  message:
+ *                    type: string
+ *                    default: Succesfully added
+ *                  details:
+ *                    type: object
+ *                    properties:
+ *                      username:
+ *                        type: string
+ *                        pattern: "^[a-zA-Z0-9]+$"
+ *                        example: "mustmax00"
+ *                        minLength: 5
+ *                        maxLength: 32
+ *                      name:
+ *                        type: string
+ *                        pattern: "\\s[^ ]"
+ *                        example: Max Mustermann
+ *                      email:
+ *                        type: string
+ *                        format: email
+ *                        example: max.mustermann@email.tld
+ *                      phonenumber:
+ *                        type: string
+ *                        example: +49 0000 0000000
+ *        "400":
+ *          $ref: "#/components/responses/InvalidInput"
+ *        "401":
+ *          $ref: "#/components/responses/Unauthorized"
+ *        "500":
+ *          $ref: "#/components/responses/InternalServerError"
+ *      security:
+ *        - userLoggedIn: []
+ *
+ */
+
 export default async (req: Request, res: Response) => {
     // Create validation schema
     const schema = Joi.object({
@@ -55,10 +124,12 @@ export default async (req: Request, res: Response) => {
     if (!Array.isArray(result) && result.affectedRows > 0) {
         // Return 200
         res.status(200).json({
-            message: "Succesfully updated",
+            message: "Succesfully added",
             details: {
-                username: validationResult.value.username,
-                adminState: validationResult.value.adminState,
+                username: validationResult.value.user.username,
+                name: validationResult.value.name,
+                email: validationResult.value.email,
+                phonenumber: validationResult.value.phonenumber,
             },
         });
         return;
@@ -68,8 +139,10 @@ export default async (req: Request, res: Response) => {
     res.status(500).json({
         error: "Could not update",
         details: {
-            username: validationResult.value.username,
-            adminState: validationResult.value.adminState,
+            username: validationResult.value.user.username,
+            name: validationResult.value.name,
+            email: validationResult.value.email,
+            phonenumber: validationResult.value.phonenumber,
         },
     });
 };
